2507e4c0e17dfcd0e1859873e84b9a9ac7bfbae1
Ex-Machin <grinpigrin@gmail.com>
2026-06-13 08:41:35 +0200

    Implemented basic minimal API for task manager

---
+++ TaskManager/Controllers/TasksController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+
+        static private List<MyTask> tasks = new List<MyTask>
+        {
+            new MyTask
+            {
+                Id =  1, // auto increment
+                Title = "Read Book",
+                Description = "Head First Design Patterns",
+                Status = Status.Created
+            },
+            new MyTask
+            {
+                Id =  2, // auto increment
+                Title = "Read Book",
+                Description = "Cracking the coding Interview",
+                Status = Status.Created
+            }
+        };
+
+
+        [HttpGet]
+        public ActionResult<List<MyTask>> Get()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<MyTask> GetByID(int id)
+        {
+            MyTask task = tasks.FirstOrDefault(task => task.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public ActionResult<MyTask> Post(MyTask newTask)
+        {
+            if (newTask == null)
+            {
+                return BadRequest();
+            }
+
+            tasks.Add(newTask);
+
+            return CreatedAtAction(nameof(Post), new {id=newTask.Id});
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, MyTask newTask)
+        {
+            MyTask task = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.Id = newTask.Id;
+            task.Title = newTask.Title;
+            task.Description = newTask.Description;
+            task.Status = newTask.Status;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            MyTask task = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            tasks.Remove(task);
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ TaskManager/Models/MyTask.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace TaskManager.Models
+{
+    public enum Status
+    {
+        Created,
+        InProgress,
+        Completed
+    }
+    public class MyTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public Status Status { get; set; }
+    }
+}
